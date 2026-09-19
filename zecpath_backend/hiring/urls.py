352e5de0f9dca2b66692ec0827407a5172443922
Ditty Xavier <dittyxavier999@gmail.com>
2026-09-19 09:53:53 +0530

    Complete Day 5 DRF APIs

--- zecpath_backend/hiring/urls.py
+++ zecpath_backend/hiring/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import JobListAPIView, UserTestAPIView
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("jobs/", views.jobs, name="jobs"),
+    path("jobs/", JobListAPIView.as_view(), name="job-list"),
+    path("users/test/", UserTestAPIView.as_view(), name="user-test"),
 ]
--- zecpath_backend/hiring/views.py
+++ zecpath_backend/hiring/views.py
@@ -1,8 +1,11 @@
 import json
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from django.http import HttpResponse, JsonResponse
 from .models import Job
-
+from .serializers import JobSerializer
 
 def home(request):
     return HttpResponse("Welcome to Zecpath Hiring API")
@@ -39,3 +42,40 @@ def jobs(request):
             "description": job.description,
             "created_at": job.created_at,
         })
+
+class JobListAPIView(APIView):
+
+    def get(self, request):
+        jobs = Job.objects.all()
+        serializer = JobSerializer(jobs, many=True)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+    def post(self, request):
+        serializer = JobSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+class UserTestAPIView(APIView):
+
+    def get(self, request):
+        return Response(
+            {
+                "message": "User API is working",
+                "user": "Test User"
+            },
+            status=status.HTTP_200_OK
+        )
